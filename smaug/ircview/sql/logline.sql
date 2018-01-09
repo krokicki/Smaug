53922e58e3210e66381bfa644fd05ff79453f0db
Konrad Rokicki <krokicki@gmail.com>
2018-01-09 02:20:08 +0000

    removed unnecessary MYISAM alters

--- smaug/ircview/sql/logline.sql
+++ smaug/ircview/sql/logline.sql
@@ -1,7 +1,5 @@
-alter table ircview_logline engine=MYISAM;
 alter table ircview_logline modify column body longtext character set utf8 collate utf8_general_ci not null;
 alter table ircview_logline modify column handle varchar(64) character set utf8 collate utf8_general_ci null;
 create fulltext index logline_ftidx on ircview_logline (body);
-
 create index logline_year_month on ircview_logline (year,month);
 
--- smaug/ircview/sql/quotelink.sql
+++ smaug/ircview/sql/quotelink.sql
@@ -1,2 +1,1 @@
-alter table ircview_quotelink engine=MYISAM;
 create fulltext index quotelink_ftidx on ircview_quotelink (match_text);
